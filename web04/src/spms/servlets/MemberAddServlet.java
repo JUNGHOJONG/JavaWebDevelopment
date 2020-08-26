98fded58029f4633f28a8c550b3bb1749d5b31f8
JUNGHOJONG <hojong1351@naver.com>
2020-08-26 22:25:09 +0900

    #1 4.6 redirect까지 완료

---
+++ web04/src/spms/servlets/MemberAddServlet.java
@@ -0,0 +1,79 @@
+package spms.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/add")
+@SuppressWarnings("serial")
+public class MemberAddServlet extends HttpServlet {
+	@Override
+	protected void doGet( HttpServletRequest request, HttpServletResponse response )
+			throws ServletException, IOException {
+		response.setContentType( "text/html; charset=utf-8" );
+		PrintWriter out = response.getWriter();	
+		out.println( "<html><head><title>회원 등록</title></head><body>" );
+		out.println( "<h1>회원 등록</h1>" );
+		out.println( "<form action='add' method='post'>" );
+		out.println( "이름: <input type='text' name='name'><br>" );
+		out.println( "이메일: <input type='text' name='email'><br>" );
+		out.println( "암호: <input type='password' name='password'><br>" );
+		out.println( "<input type='submit' value='추가'>" );
+		out.println( "<input type='reset' value='취소'>" );
+		out.println( "</form>" );
+		out.println( "</body></html>" );
+	}
+	
+	@Override
+	protected void doPost( HttpServletRequest request, HttpServletResponse response )
+			throws ServletException, IOException {
+		request.setCharacterEncoding( "UTF-8" );
+		Connection connection = null; 
+		PreparedStatement statement = null;
+		try {
+			// 1. JDBC 드라이버 등록
+			DriverManager.registerDriver( new com.mysql.jdbc.Driver() );
+			
+			// 2. JDBC 드라이버와 MySQL서버 연결
+			String timeZoneValue = "useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+			connection = DriverManager.getConnection( "jdbc:mysql://localhost/madang?" + timeZoneValue,
+					"madang", "madang" );
+			
+			// 3. 커넥션 객체로부터 SQL을 던질 객체를 준비
+ 			statement = connection.prepareStatement( "Insert INTO MEMBERS( EMAIL, PWD, MNAME, CRE_DATE, MOD_DATE )"
+ 					+ " VALUES( ?, ?, ?, NOW(), NOW() )" );
+ 			statement.setString( 1, request.getParameter( "email" ) );
+ 			statement.setString( 2, request.getParameter( "password" ) );
+ 			statement.setString( 3, request.getParameter( "name" ) );
+			
+ 			// 4. SQL을 던질 객체로 서버에 질의
+ 			statement.executeUpdate();
+ 			
+ 			response.sendRedirect( "list" ); // redirect
+ 			
+ 			// 5. 서버에서 가지고 온 데이터로 HTML을 작성하여 웹 브라우저로 출력 
+ 			response.setContentType( "text/html; charset=utf-8" );
+ 			PrintWriter out = response.getWriter();
+ 			out.println( "<html><head><title>회원등록결과</title>" );
+ 			out.println( "<meta http-equiv='Refresh' content='100; url=list'>" ); // refresh
+ 			out.println( "</head><body>" );
+ 			out.println( "<p>등록 성공입니다!!</p>" );
+ 			out.println( "</body></html>" );
+
+		} catch ( Exception e ) {
+			throw new ServletException( e );
+		} finally {
+ 			try { statement.close(); } catch( Exception e ) {}
+ 			try { connection.close(); } catch( Exception e ) {}
+		}
+	}
+	
+}
---
+++ web04/src/spms/servlets/MemberListServlet.java
@@ -0,0 +1,76 @@
+package spms.servlets;
+
+import java.io.IOException;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet( "/member/list" )
+public class MemberListServlet extends GenericServlet implements Servlet{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service( ServletRequest request, ServletResponse response ) throws ServletException, IOException{
+		/*
+		   1. 사용할 JDBC 드라이버를 등록하라.
+		   2. 드라이버를 사용하여 MySQL서버와 연결하라.
+		   3. 커넥션 객체로부터 SQL를 던질 객체를 준비하라.
+		   4. SQL을 던지는 객체를 사용하여 서버에 질의하라.
+		   5. 서버에 가져온 데이터를 사용하여 HTML을 만들어서 웹 브라우저로 출력하라. 
+		*/
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		try {
+			
+			// 1. 사용할 JDBC 드라이버를 등록하라.
+			DriverManager.registerDriver( new com.mysql.jdbc.Driver() );
+			
+			// 2. 드라이버를 사용하여 MySQL서버와 연결하라.
+			String timeZoneValue = "useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+			connection = DriverManager.getConnection( 
+					"jdbc:mysql://localhost/madang?" + timeZoneValue,
+					"madang", "madang" );
+			
+			// 3. 커넥션 객체로부터 SQL를 던질 객체를 준비하라.
+			statement = connection.createStatement();
+			
+			// 4. SQL을 던지는 객체를 사용하여 서버에 질의하라.
+			resultSet = statement.executeQuery( // resultSet 객체 -> 서버에서 데이터를 가지고 오는 객체(O), 서버에서 가지고 온 데이터를 가지고 있는 객체(X)
+			"SELECT MNO,MNAME,EMAIL,CRE_DATE" +
+			" FROM MEMBERS" +
+			" ORDER BY MNO ASC" );
+			
+			// 5. 서버에 가져온 데이터를 사용하여 HTML을 만들어서 웹 브라우저로 출력하라.
+			response.setContentType( "text/html; charset=UTF-8" );
+			PrintWriter out = response.getWriter();
+			out.println( "<html><head><title>회원목록</title></head>" );
+			out.println( "<body><h1>회원목록</h1>" );
+			out.println( "<p><a href='add'>신규 회원</a></p>" );
+			while( resultSet.next() ) {
+				out.println( resultSet.getInt( "MNO" ) + "," +
+			resultSet.getString( "MNAME" ) + "," + 
+			resultSet.getString( "EMAIL" ) + "," + 
+			resultSet.getDate( "CRE_DATE" ) + "<br>" );
+			}
+			out.println( "</body></html>" );
+		}catch( SQLException e ) {
+			throw new ServletException( e );
+		}finally {
+			try { if( resultSet != null ) resultSet.close(); } catch( Exception e ) {}
+			try { if( statement != null ) statement.close(); } catch( Exception e ) {}
+			try { if( connection != null ) connection.close(); } catch( Exception e ) {}
+		}
+	}
+}
