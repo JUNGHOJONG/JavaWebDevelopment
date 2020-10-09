91c41bb0a7773de424de1867da7570fdb3ee2251
JUNGHOJONG <hojong1351@naver.com>
2020-10-09 17:13:32 +0900

    #3 7.6 동적 SQL의 사용까지 완료

--- web07/src/spms/dao/MysqlProjectDao.java
+++ web07/src/spms/dao/MysqlProjectDao.java
@@ -1,5 +1,6 @@
 package spms.dao;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -16,13 +17,12 @@ public class MysqlProjectDao implements ProjectDao {
 	}
 
 	@Override
-	public List<Project> selectList() throws Exception {
+	public List<Project> selectList(HashMap<String, Object> map) throws Exception {
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 		try {
-			return sqlSession.selectList("spms.dao.ProjectDao.selectList");	
+			return sqlSession.selectList("spms.dao.ProjectDao.selectList", map);	
 		}finally {
 			sqlSession.close();
-		
 		}
 	}
 
@@ -50,9 +50,34 @@ public class MysqlProjectDao implements ProjectDao {
 	public int update(Project project) throws Exception {
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 		try {
-			int count = sqlSession.update("spms.dao.ProjectDao.update", project);
-			sqlSession.commit();
-			return count;
+			Project originalProject = selectOne(project.getNo());
+			HashMap<String, Object> parameterMap = new HashMap<>();
+			if(!originalProject.getTitle().equals(project.getTitle())) {
+				parameterMap.put("title", project.getTitle());	
+			}
+			if(!originalProject.getContent().equals(project.getContent())) {
+				parameterMap.put("content", project.getContent());
+			}
+			if(originalProject.getStartDate().compareTo(project.getStartDate()) != 0) {
+				parameterMap.put("startDate", project.getStartDate());
+			}
+			if(originalProject.getEndDate().compareTo(project.getEndDate()) != 0) {
+				parameterMap.put("endDate", project.getEndDate());
+			}
+			if(originalProject.getState() != project.getState()) {
+				parameterMap.put("state", project.getState());
+			}
+			if(!originalProject.getTags().equals(project.getTags())) {
+				parameterMap.put("tags", project.getTags());
+			}
+			if(parameterMap.size() > 0) {
+				parameterMap.put("no", project.getNo());
+				int count = sqlSession.update("spms.dao.ProjectDao.update", parameterMap);
+				sqlSession.commit();
+				return count;	
+			}else {
+				return 0;
+			}
 		}finally {
 			sqlSession.close();
 		}
